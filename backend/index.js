aa5cb9bf5014aaa3c7d89af582b2d1ff9119ffba
unknown <dattatreya.chakraborty12@gmail.com>
2025-08-01 19:23:33 +0530

    url change

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 app.use(
     cors({
-        origin: ["http://localhost:5173", "https://pixella-app.vercel.app/"],
+        origin: ["http://localhost:5173", "https://pixella-app.vercel.app"],
         credentials: true,
     }),
 );
