dc857084edba944535a001c4797e67fb10e8a5e4
unknown <dattatreya.chakraborty12@gmail.com>
2025-08-01 19:29:08 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 app.use(
     cors({
-        origin: ["http://localhost:5173", "https://pixella-app.vercel.app"],
+        origin: ["https://pixella-app.vercel.app", "http://localhost:5173"],
         credentials: true,
     }),
 );
